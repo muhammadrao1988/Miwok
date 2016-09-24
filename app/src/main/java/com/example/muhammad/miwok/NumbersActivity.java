5d6786da76d1d1364915bf05f07bfbbe9c67afaa
Muhammad Rao <muhammadrao1988@gmail.com>
2016-09-24 22:51:52 +0500

    Fixed fragment issue

--- app/src/main/java/com/example/muhammad/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/muhammad/miwok/NumbersActivity.java
@@ -50,14 +50,8 @@ public class NumbersActivity extends AppCompatActivity {
     };
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, new NumbersFragment())
-                .commit();
-    }
-    protected void onCreate2(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
         //array of words
